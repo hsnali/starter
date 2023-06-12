afafe2fa16f0d735aade8373175e69ea80edae69
Hassan Ali <hassan.ali@empired.com>
2023-06-12 15:18:35 +1000

    remove css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import './App.css'
-
 import { Button, Footer, Header } from '@/components'
 
 function App() {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-stone-50 p-6">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-stone-50 p-6 font-sans dark:bg-slate-800 dark:text-white">
       <Header />
       <Button />
       <Footer />

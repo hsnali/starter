892d9da23929856ac5a8fbf346e06c1b2cf6dc8b
Hassan Ali <hassan.ali@empired.com>
2023-06-12 12:11:00 +1000

    extract button comp

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const Button = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
+    </button>
+  );
+};

e6a7c10dad5bdd9ba5b3c6d05af735b25536a423
Hassan Ali <hassan.ali@empired.com>
2023-06-24 18:22:57 +1000

    add coverage steps

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,8 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
+    '@storybook/addon-coverage'
   ],
   framework: {
     name: '@storybook/react-vite',

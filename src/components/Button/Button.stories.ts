0306dac5b93c0fd586725b9d99c1c1f8a4d4ef65
Hassan Ali <hassan.ali@empired.com>
2023-06-25 12:24:07 +1000

    add interaction test

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -1,13 +1,27 @@
+import { expect } from '@storybook/jest'
 import type { Meta, StoryObj } from '@storybook/react'
+import { userEvent, within } from '@storybook/testing-library'
 
 import { Button } from './Button'
 
 const meta = {
-  component: Button,
-  tags: ['autodocs']
+  component: Button
 } satisfies Meta<typeof Button>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Primary: Story = {}
+export const Primary: Story = {
+  play: async ({ canvasElement, step }) => {
+    const button = await within(canvasElement).findByRole('button')
+
+    await step('Should have label', async () => {
+      expect(button).toHaveTextContent('Count is 0')
+    })
+
+    await step('Should increment count on click', async () => {
+      await userEvent.click(button)
+      expect(button).toHaveTextContent('Count is 1')
+    })
+  }
+}

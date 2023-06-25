5009dc3c1756c321c709ad847f1a6376cb27f586
Hassan Ali <hassan.ali@empired.com>
2023-06-25 16:43:38 +1000

    cat fact util

---
+++ src/utils/getCatFact.ts
@@ -0,0 +1,18 @@
+export const CAT_FACT_API = 'https://catfact.ninja/fact'
+
+export type CatFactReponse = {
+  fact: string
+  length: number
+}
+
+export const getCatFact = async (apiUrl = CAT_FACT_API): Promise<CatFactReponse> => {
+  const response = await fetch(apiUrl)
+  const { fact, length } = await response.json()
+
+  console.log('get fact', fact)
+  if (!response.ok) {
+    return Promise.reject(new Error('Failed to fetch cat fact'))
+  }
+
+  return { fact, length }
+}

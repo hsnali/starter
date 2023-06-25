88bac96901e563a7a90fedb0e38fd2963cad5df5
Hassan Ali <hassan.ali@empired.com>
2023-06-26 08:30:39 +1000

    server setup

---
+++ src/tests/setup.ts
@@ -0,0 +1,12 @@
+// tests/setup.ts
+import { fetch } from 'cross-fetch'
+import { afterAll, afterEach, beforeAll } from 'vitest'
+
+import { server } from './server'
+
+// Add `fetch` polyfill.
+global.fetch = fetch
+
+beforeAll(() => server.listen({ onUnhandledRequest: `error` }))
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())

e21b38c9fc28ca12f8f6e149755c6849a9eb7d29
Hassan Ali <hassan.ali@empired.com>
2023-06-27 18:28:53 +1000

    set dark mode

--- src/utils/setDarkMode.ts
+++ src/utils/setDarkMode.ts
@@ -1,10 +1,11 @@
-const DARK_CLASS = 'dark'
+export const THEME_KEY = 'theme'
+export const DARK_CLASS = 'dark'
 export const setDarkMode = (darkMode = false) => {
   if (darkMode) {
     document.documentElement.classList.add(DARK_CLASS)
-    localStorage.setItem('theme', DARK_CLASS)
+    localStorage.setItem(THEME_KEY, DARK_CLASS)
   } else {
     document.documentElement.classList.remove(DARK_CLASS)
-    localStorage.removeItem('theme')
+    localStorage.removeItem(THEME_KEY)
   }
 }

fd418b679e04a2b6ccd424d6de7ccb55c68a3f19
Hassan Ali <hassan.ali@empired.com>
2023-07-02 12:12:04 +1000

    move app

--- src/App.stories.ts
+++ src/App.stories.ts
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker'
 import type { Meta, StoryObj } from '@storybook/react'
 
 import App from '@/App'
-import { withLocalStorage, withReactQuery } from '@/stories/decorators'
+import { useWithReactQuery, withLocalStorage } from '@/stories/decorators'
 import { mockCatFact } from '@/tests/handlers/mockCatFact'
 import { DARK_CLASS, THEME_KEY } from '@/utils/setDarkMode'
 
@@ -14,7 +14,7 @@ const meta = {
       [THEME_KEY]: null
     }
   },
-  decorators: [withLocalStorage, withReactQuery]
+  decorators: [withLocalStorage, useWithReactQuery]
 } satisfies Meta<typeof App>
 
 export default meta

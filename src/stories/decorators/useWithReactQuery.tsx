8b1adb235c797b7e72b47d3e30f5e5068add169d
Hassan Ali <hassan.ali@empired.com>
2023-07-02 12:12:13 +1000

    use naming convention

--- src/stories/decorators/useWithReactQuery.tsx
+++ src/stories/decorators/useWithReactQuery.tsx
@@ -4,16 +4,15 @@ import { useEffect } from 'react'
 
 import { queryClient, QueryProvider } from '@/providers/QueryProvider'
 
-export const withReactQuery: Decorator = (Story, { parameters }) => {
+export const useWithReactQuery: Decorator = (Story, { parameters }) => {
   // Reset react query state between stories
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
-    queryClient.resetQueries()
+    queryClient.clear()
   })
 
   return (
     <QueryProvider>
-      {Story()}
+      <Story />
       {parameters?.queryDevTools && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryProvider>
   )

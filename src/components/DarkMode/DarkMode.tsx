d3b190797bb2647d960730cbc52a5d9051bb6906
Hassan Ali <hassan.ali@empired.com>
2023-07-02 13:28:41 +1000

    move logic to provider

--- src/components/DarkMode/DarkMode.tsx
+++ src/components/DarkMode/DarkMode.tsx
@@ -1,24 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useContext } from 'react'
 
 import { ReactComponent as MoonIcon } from '@/assets/icons/moon.svg'
 import { ReactComponent as SunIcon } from '@/assets/icons/sun.svg'
-import { DARK_CLASS, setDarkMode, THEME_KEY } from '@/utils/setDarkMode'
+import { ThemeContext } from '@/providers/ThemeProvider'
 
 export const DarkMode = () => {
-  const savedTheme = localStorage.getItem(THEME_KEY)
-  const [isDark, setIsDarkMode] = useState(savedTheme === DARK_CLASS)
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDark)
-    setDarkMode(!isDark)
-  }
-
-  useEffect(() => {
-    setDarkMode(isDark)
-  })
+  const { toggleDarkMode, isDark } = useContext(ThemeContext)
 
   return (
-    <button onClick={toggleDarkMode} className="absolute right-4 top-4 z-10 rounded-full p-1">
+    <button onClick={() => toggleDarkMode()} className="absolute right-4 top-4 z-10 rounded-full p-1">
       <span className="sr-only">Toggle Dark Mode</span>
       {isDark ? (
         <MoonIcon data-testid="dark-mode-moon" className="text-gray-100" />

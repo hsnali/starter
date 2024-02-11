f00dd3956200fafad7238d4cbf8707bcf222773b
Hassan Ali <hello@hassan.so>
2024-02-11 20:16:10 +1100

    Refactor withTheme decorator to simplify theme assignment

--- src/stories/decorators/withTheme.tsx
+++ src/stories/decorators/withTheme.tsx
@@ -6,8 +6,7 @@ import { DARK_CLASS, THEME_KEY } from '@/utils/setDarkMode'
 export const withTheme: Decorator = (Story, { globals, parameters }) => {
   const isDarkGlobal = globals['theme'] === DARK_CLASS
   const isDarkParam = parameters.localStorage ? parameters.localStorage[THEME_KEY] === DARK_CLASS : false
-  const isDark = isDarkGlobal || isDarkParam
-  const theme = isDark ? DARK_CLASS : ''
+  const theme = isDarkGlobal || isDarkParam ? DARK_CLASS : ''
 
   // Override localStorage - workaroudn for withThemes being high order than withLocalStorage
   window.localStorage.setItem(THEME_KEY, theme)
